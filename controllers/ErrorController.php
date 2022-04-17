cf6cd44df7d71691a258edc2627f21e6402dd818
Andres Sanchez <adsanchez@uniguajira.edu.co>
2022-04-17 10:34:20 -0500

    Se creo un nuevo error 403

--- controllers/ErrorController.php
+++ controllers/ErrorController.php
@@ -6,4 +6,9 @@ class ErrorController{
         //invocamos la vista
         require_once 'views/error/404.php';
     }
+
+    public function Error403(){
+        //invocamos la vista
+        require_once 'views/error/403.php';
+    }
 }
