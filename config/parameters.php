b0cf79041e0393b53b637028900843d5f30e6ea6
Andres Sanchez <adsanchez@uniguajira.edu.co>
2022-04-17 10:29:27 -0500

    Se creo una funcion para permitir usar google recaptcha

--- config/parameters.php
+++ config/parameters.php
@@ -2,5 +2,6 @@
 define('base_url','http://localhost/ModelMvc/');
 define('controller_default','IndexController');
 define('action_default','Index');
+define('CLAVE_SECRETA','asdosijdfosi');
 
 date_default_timezone_set('America/Bogota'); //zona horaria colombiana
